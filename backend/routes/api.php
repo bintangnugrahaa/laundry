c2c9966d6302b335f419217620a47d9be80773b6
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-06-18 10:55:43 +0700

    read all tables

---
+++ backend/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\api\LaundryController;
+use App\Http\Controllers\api\PromoController;
+use App\Http\Controllers\api\ShopController;
+use App\Http\Controllers\api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/promo', [PromoController::class, 'readAll']);
+Route::get('/shop', [ShopController::class, 'readAll']);
+Route::get('/laundry', [LaundryController::class, 'readAll']);
+Route::get('/user', [UserController::class, 'readAll']);
