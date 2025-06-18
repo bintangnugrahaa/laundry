085dc1e3fac763a8a5f85c9384f79789e88e3293
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-06-18 11:09:58 +0700

    read shop recommendation limit

--- backend/app/Http/Controllers/api/ShopController.php
+++ backend/app/Http/Controllers/api/ShopController.php
@@ -16,4 +16,22 @@ class ShopController extends Controller
             'data' => $shops,
         ], 200);
     }
+
+    function readRecommendationLimit()
+    {
+        $shops = Shop::orderBy('rate', 'desc')
+            ->limit(5)
+            ->get();
+
+        if (count($shops) > 0) {
+            return response()->json([
+                'data' => $shops,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'not found',
+                'data' => $shops,
+            ], 404);
+        }
+    }
 }
