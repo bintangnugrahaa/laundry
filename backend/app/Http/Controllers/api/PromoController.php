64bb1a9c692a44346d394683261d85942154d0b2
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-06-18 11:06:41 +0700

    read promo limit

--- backend/app/Http/Controllers/api/PromoController.php
+++ backend/app/Http/Controllers/api/PromoController.php
@@ -16,4 +16,23 @@ class PromoController extends Controller
             'data' => $promos,
         ], 200);
     }
+
+    public function readLimit()
+    {
+        $promos = Promo::orderBy('created_at', 'desc')
+            ->limit(5)
+            ->with('shop')
+            ->get();
+
+        if ($promos->count() > 0) {
+            return response()->json([
+                'data' => $promos,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Not found',
+                'data' => $promos,
+            ], 404);
+        }
+    }
 }
